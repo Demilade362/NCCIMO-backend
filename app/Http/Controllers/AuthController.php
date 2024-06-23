ab10b84faf24acaa3a1a15e00db9e67f8c3b1196
Demilade362 <ademolademilade362@gmail.com>
2024-06-23 22:04:45 +0100

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            abort(404, 'User Not Found');
+        }
+
+        return response([
+            'user' => $user,
+            'token' => $user->createToken($user->email)->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        return $request->user()->currentAccessToken()->delete();
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+            'password_confirmation' => 'required',
+            'number' => 'required',
+            'zone' => 'required|string',
+            'subzone' => 'required|string',
+            'state' => 'required',
+            'stateCode' => 'required'
+        ]);
+
+        $user = User::create($request->all());
+
+        return response([
+            'user' => $user,
+            'token' => $user->createToken($user->email)->plainTextToken
+        ]);
+    }
+}
---
+++ resources/views/mail/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-mail::message>
+# Welcome Message
+
+NCCFIMO welcomes you to the body of chirst, we hope you are doing fine.
+God bless you.
+
+<x-mail::button :url="'https://nccfimo.org.ng/'">
+Back To Site
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
